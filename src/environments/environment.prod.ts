c6b011b00604975c8c70d3afee571fe1feb66f84
Sofia Carballo <sofiacarballo1990@gmail.com>
2022-01-20 15:00:45 -0300

    update enviroment.product

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  DEFAULT_LANG: `es`,
-  production: false,
-  API_PREFIX: ``,
-  API_HOST: `localhost`,
-  API_PORT:`:4200`,
-  API_PROTOCOL: `http://`,
+  production: true,
   /* production: false,
   API_PREFIX: `/api`,
   API_HOST: `dominio`,
   API_PORT:``, //queda vacio
   API_PROTOCOL: `https://`, */
+  DEFAULT_LANG: `es`,
+  API_PREFIX: ``,
+  API_HOST: `localhost`,
+  API_PORT:`:4200`,
+  API_PROTOCOL: `http://`,
 };
