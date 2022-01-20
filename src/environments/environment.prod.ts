1a8974f041ffd7e80d8af66a3bce7c5179d4f97d
Sofia Carballo <sofiacarballo1990@gmail.com>
2022-01-20 14:36:56 -0300

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  DEFAULT_LANG: `es`,
+  production: false,
+  API_PREFIX: ``,
+  API_HOST: `localhost`,
+  API_PORT:`:4200`,
+  API_PROTOCOL: `http://`,
+  /* production: false,
+  API_PREFIX: `/api`,
+  API_HOST: `dominio`,
+  API_PORT:``, //queda vacio
+  API_PROTOCOL: `https://`, */
 };
